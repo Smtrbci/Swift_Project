4b687c69a3c50dc0e0fd225ae9bbf6dfef6586bc
Samet Arabacı <sametarabacitr@gmail.com>
2024-10-23 21:04:44 +0300

    23.10.2024

---
+++ LayoutApp/LayoutApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LayoutApp
+//
+//  Created by Samet Arabacı on 3.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
