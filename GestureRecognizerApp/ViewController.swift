f07d37d1d71781cdad83d9f50fa9f56c52cbe881
Samet Arabacı <samet.arabaci@mlpcare.com>
2024-10-23 14:53:34 +0300

    Initial Commit

---
+++ GestureRecognizerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GestureRecognizerApp
+//
+//  Created by Samet Arabacı on 23.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
