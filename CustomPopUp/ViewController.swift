7854be8d20370b42f69f15f97cba022ee7135a91
Samet Arabacı <sametarabacitr@gmail.com>
2024-10-31 11:52:10 +0300

    Initial Commit

---
+++ CustomPopUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomPopUp
+//
+//  Created by Samet Arabacı on 31.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
