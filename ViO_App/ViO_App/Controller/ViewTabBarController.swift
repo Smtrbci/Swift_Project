e63927ed3dd411de73f2be1388729a338f2b4c6d
Samet Arabacı <sametarabacitr@gmail.com>
2025-05-24 15:50:47 +0300

    ViO_App klasörü normal klasör olarak eklendi

---
+++ ViO_App/ViO_App/Controller/ViewTabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewTabBarController.swift
+//  ViO_App
+//
+//  Created by Samet Arabacı on 19.02.2025.
+//
+
+import UIKit
+
+class ViewTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.selectedIndex = 1
+    }
+}
